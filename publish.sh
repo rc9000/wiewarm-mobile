c0b67d65700576d7362f0ddfb7be49b000f42f3e
Christian Ramseyer <ramseyer@netnea.com>
2021-03-20 18:20:03 +0100

    move publish.sh

--- publish.sh
+++ publish.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd $DIR/app
 DEST=/home/wiewarmmobile/public_html/
 npm run-script build
 scp -r build/* exile.networkz.ch:$DEST
